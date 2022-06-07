859fa89ef250a979615116d36fdf91e7490ffbd2
deanishw <106957579+deanishw@users.noreply.github.com>
2022-06-07 21:15:00 +0700

    Update Grafik Hujan Bulan Maret.py

--- Grafik Hujan Bulan Maret.py
+++ Grafik Hujan Bulan Maret.py
@@ -1,3 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 3 15:04:51 2022
+
+@author: Amalia, Deanis, Keisha, Marissa, Yuannisa
+"""
+
+'''KEISHA'''
+
+#Program Membuat Grafik Curah Hujan Bulan Maret
+
 #import library yang dibutuhkan
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -15,6 +26,8 @@ df[['Tahun','Bulan','Tanggal']]=df["Tanggal-Bulan-Tahun"].str.split('-',expand=T
 T1 = df['Tanggal']
 T2 = T1.values.tolist()
 Tf = list(dict.fromkeys(T2))
+
+'''MARISSA'''
 
 #memilah dan merata-ratakan data sesuai dengan tanggal yang sama
 T3 = df.groupby(['Tanggal-Bulan-Tahun']).mean()
@@ -36,10 +49,10 @@ print(tabel)
 fig = plt.figure(figsize=(15,4), dpi=200)
 plt.plot(Tf,Df, label="Data Curah Hujan")
 
-plt.xlabel("Tanggal")
-plt.ylabel("Data")
+plt.xlabel("Tanggal", size=12)
+plt.ylabel("Data", size=12)
 
-plt.title("Grafik Curah Hujan Bulan Maret 2022")
+plt.title("Grafik Curah Hujan Bulan Maret 2022", size=16)
 plt.grid(True)
 
 plt.legend()
